3a8caa37534dd1990d2fa5dacef53cc65bcf585c
CeciliaGnz <ceciliasoto200309@gmail.com>
2024-05-26 17:31:22 -0500

    Update profile section edit name, edit pass and view the name

--- Parcial #2 - Income and Expenses system/js/scriptProfile.js
+++ Parcial #2 - Income and Expenses system/js/scriptProfile.js
@@ -1,28 +1,89 @@
-((Sesion) =>{
-    const App = {
-        htmlElements: {
-          logoutButton: document.getElementById("logoutButton")
-        },
-    init() {
-        App.bindEvents();
-        App.verificarSesion();
-      },
 
-      verificarSesion() {
-        Sesion.estadoNoLoggeado();
-      },
+  ((Sesion) => {
+      const App = {
+          htmlElements: {
+              logoutButton: document.getElementById("logoutButton"),
+              passNewBtn: document.getElementById("passNewBtn"),
+              nameNewBtn:document.getElementById("nameNewBtn"),
+              userName: document.getElementById("userName"),
+              nombre: document.getElementById("nombre"),
+              titleNom: document.getElementById("titleNom"),
+              newPass: document.getElementById("newPass"),
+              confirmPass: document.getElementById("confirmPass")
+          },
+          init() {
+              App.bindEvents();
+              App.verificarSesion();
+              App.mostrarData();
+          },
 
-      bindEvents() {
-        App.htmlElements.logoutButton.addEventListener("click", App.handlers.onLogout);
-      },
-      handlers: {
-        onLogout(e) {
-          e.preventDefault();
-          Sesion.logout();
-        },
-      },
-    };
-    
-    App.init();
-    
-})(window.Sesion);
+          verificarSesion() {
+              Sesion.estadoNoLoggeado();
+          },
+
+          bindEvents() {
+                App.htmlElements.logoutButton.addEventListener("click", App.handlers.onLogout);
+                App.htmlElements.nameNewBtn.addEventListener("click", App.Edit.editName);
+                App.htmlElements.passNewBtn.addEventListener("click", App.Edit.editPass);
+          },
+          handlers: {
+              onLogout(e) {
+                  e.preventDefault();
+                  Sesion.logout();
+              }
+          },
+
+          Edit:{
+            editName(e){
+              e.preventDefault();
+              const nombre=App.htmlElements.nombre.value;
+              App.methods.cambiarNombre(nombre)
+            },
+
+            editPass(e) {
+              e.preventDefault();
+              const newPass = App.htmlElements.newPass.value;
+              const confirmPass = App.htmlElements.confirmPass.value;
+              if (newPass === confirmPass) {
+                  App.methods.cambiarPassword(newPass);
+              } else {
+                  alert("Las contraseñas no coinciden.");
+              }
+          }
+          },
+
+          mostrarData() {
+              App.methods.showUserName();
+              App.methods.showName();
+          },
+
+          methods: {
+              showUserName() {
+                  const username = localStorage.getItem("username");
+                  if (username && App.htmlElements.userName) {
+                      App.htmlElements.userName.value = username;
+                  }
+              },
+              showName() {
+                  const nombre = localStorage.getItem("nombre");
+                  if (nombre && App.htmlElements.nombre) {
+                      App.htmlElements.nombre.value = nombre;
+                      App.htmlElements.titleNom.textContent=nombre;
+                  }
+              },
+
+              cambiarNombre(nuevoNombre) {
+                Sesion.cambiarNombre(nuevoNombre);
+                App.methods.showName(); 
+                alert("Nombre modificado.");
+              },
+
+              cambiarPassword(newPass){
+                Sesion.cambiarPass(newPass);
+                alert("Contraseña modificada.");
+              }
+          }
+      };
+
+      App.init();
+  })(window.Sesion);
