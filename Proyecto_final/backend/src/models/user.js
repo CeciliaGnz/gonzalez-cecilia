1e57cce33c100bfdd4c039e51e90e5312111e645
Cecilia González <102307413+CeciliaGnz@users.noreply.github.com>
2024-07-11 19:31:23 +0000

    Test register

--- Proyecto_final/backend/src/models/user.js
+++ Proyecto_final/backend/src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -16,6 +16,6 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
-const User = mongoose.model('User', userSchema, 'users');
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
