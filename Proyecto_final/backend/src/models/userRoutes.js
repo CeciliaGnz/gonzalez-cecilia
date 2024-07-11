9a12ebf8073ea6d38ffc87744146b5fb8161dba9
Sherlein C. <sherleinc.@MacBook-Air-de-Sherlein.local>
2024-07-11 12:54:59 -0500

    Subiendo cambios de bcryptjs

--- Proyecto_final/backend/src/models/userRoutes.js
+++ Proyecto_final/backend/src/models/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Función para validar la complejidad de la contraseña
 function validatePassword(password) {
